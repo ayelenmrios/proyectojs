408f9394c051cbc71a74d898e79e30a12d641e3c
Ayelen Machicado <ayelenmachicado2001@gmail.com>
2023-04-11 18:57:07 -0300

    Function

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
-//ingrese nombre de la materia
-//ingrese hasta cinco notas
 
 alert("Calcule el promedio de su materia!");
 let materia = prompt("Ingrese el nombre de la materia:");
 
-let sumaNotas = 0;
-for(i = 0; i < 5; i++){
-    notas = parseInt(prompt("Ingrese hasta cinco notas:"));
-    sumaNotas = sumaNotas + notas;
-    console.log(notas);
+function cuenta(){
+    let sumaNotas = 0;
+
+    for(i = 0; i < 5; i++){
+        notas = parseInt(prompt("Ingrese hasta cinco notas:"));
+
+        sumaNotas = sumaNotas + notas;
+    }
+
+    return total = sumaNotas / 5;
 }
 
-promedio = sumaNotas / 5;
+let promedio = cuenta();
 
 if(promedio >= 6){
     alert("Felicidades! El promedio de " + materia + " es: " + promedio);
@@ -19,8 +22,5 @@ if(promedio >= 6){
     alert("Lo siento. Tu promedio de " + materia + " es: " + promedio + ". Boludeaste en semana santa.");
 }
 
-//Para comprobar las operaciones matematicas
-console.log(sumaNotas);
-console.log(promedio);
 
 
